6c4ad04b90175bde8a4515cb886699f571d79f49
taek0622 <min4644@gmail.com>
2024-08-09 09:08:05 +0900

    Chore: #1 Add TodaysPokemonViewController
    - 오늘의 포켓몬을 확인하기 위한 TodaysPokemonViewController 추가

---
+++ PokemonDex/TodaysPokemonViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TodaysPokemonViewController.swift
+//  PokemonDex
+//
+//  Created by 김민택 on 8/9/24.
+//
+
+import UIKit
+
+class TodaysPokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
