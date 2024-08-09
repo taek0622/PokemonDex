d557adddefca34142df86075d59c15675af4654a
taek0622 <min4644@gmail.com>
2024-08-09 09:33:20 +0900

    Chore: #1 Add TypeColor Enum
    - 타입 색상을 모아둔 enum 추가

---
+++ PokemonDex/TypeColor.swift
@@ -0,0 +1,26 @@
+//
+//  TypeColor.swift
+//  PokemonDex
+//
+//  Created by 김민택 on 8/9/24.
+//
+
+enum TypeColor {
+    static let fireType = #colorLiteral(red: 0.9137254902, green: 0.6274509804, blue: 0.3490196078, alpha: 1)
+    static let waterType = #colorLiteral(red: 0.4078431373, green: 0.5607843137, blue: 0.8235294118, alpha: 1)
+    static let grassType = #colorLiteral(red: 0.4980392157, green: 0.7176470588, blue: 0.3764705882, alpha: 1)
+    static let electricType = #colorLiteral(red: 0.9137254902, green: 0.8235294118, blue: 0.2862745098, alpha: 1)
+    static let iceType = #colorLiteral(red: 0.5803921569, green: 0.8, blue: 0.7529411765, alpha: 1)
+    static let fightingType = #colorLiteral(red: 0.7137254902, green: 0.2941176471, blue: 0.4156862745, alpha: 1)
+    static let poisonType = #colorLiteral(red: 0.6235294118, green: 0.431372549, blue: 0.7725490196, alpha: 1)
+    static let groundType = #colorLiteral(red: 0.768627451, green: 0.4862745098, blue: 0.2901960784, alpha: 1)
+    static let flyingType = #colorLiteral(red: 0.5960784314, green: 0.6588235294, blue: 0.8588235294, alpha: 1)
+    static let psychictype = #colorLiteral(red: 0.8705882353, green: 0.4705882353, blue: 0.4666666667, alpha: 1)
+    static let bugType = #colorLiteral(red: 0.6156862745, green: 0.7490196078, blue: 0.2274509804, alpha: 1)
+    static let rockType = #colorLiteral(red: 0.7568627451, green: 0.7176470588, blue: 0.5490196078, alpha: 1)
+    static let ghostType = #colorLiteral(red: 0.3568627451, green: 0.4117647059, blue: 0.6666666667, alpha: 1)
+    static let dragonType = #colorLiteral(red: 0.2470588235, green: 0.4196078431, blue: 0.7607843137, alpha: 1)
+    static let darkType = #colorLiteral(red: 0.3450980392, green: 0.3254901961, blue: 0.4, alpha: 1)
+    static let steelType = #colorLiteral(red: 0.4235294118, green: 0.5529411765, blue: 0.6235294118, alpha: 1)
+    static let fairyType = #colorLiteral(red: 0.8470588235, green: 0.5803921569, blue: 0.8941176471, alpha: 1)
+}
