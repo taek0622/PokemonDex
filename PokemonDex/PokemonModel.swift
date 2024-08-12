d567636aa677348c90ae0212d243a0cd072ccfea
taek0622 <min4644@gmail.com>
2024-08-12 21:47:27 +0900

    Chore: Modify PokemonSpeciesModel
    - PokemonSpeciesModel 구성 변경

--- PokemonDex/PokemonModel.swift
+++ PokemonDex/PokemonModel.swift
@@ -398,22 +398,60 @@ struct PokemonSpritesGenerationVIII: Codable {
 }
 
 struct PokemonSpeciesModel: Codable {
-    var flavorTextEntries: [FlavorText]
     var id: Int
     var name: String
-    var names: [NameModel]
     var order: Int
+    var genderRate: Int
+    var captureRate: Int
+    var baseHappiness: Int
+    var isBaby: Bool
+    var isLegendary: Bool
+    var isMythical: Bool
+    var hatchCounter: Int
+    var hasGenderDifferences: Bool
+    var formsSwitchable: Bool
+    var growthRate: NamedAPIResource
     var pokedexNumbers: [PokemonSpeciesDexEntry]
+    var eggGroups: [NamedAPIResource]
+    var color: NamedAPIResource?
+    var shape: NamedAPIResource?
+    var evolvesFromSpecies: NamedAPIResource?
+    var evolutionChain: APIResource?
+    var habitat: NamedAPIResource?
+    var generation: NamedAPIResource?
+    var names: [NameModel]
+    var flavorTextEntries: [FlavorText]
+    var formDescriptions: [Description]
     var genera: [Genus]
+    var varieties: [PokemonSpeciesVariety]
 
     enum CodingKeys: String, CodingKey {
-        case flavorTextEntries = "flavor_text_entries"
-        case genera
         case id
         case name
-        case names
         case order
+        case genderRate = "gender_rate"
+        case captureRate = "capture_rate"
+        case baseHappiness = "base_happiness"
+        case isBaby = "is_baby"
+        case isLegendary = "is_legendary"
+        case isMythical = "is_mythical"
+        case hatchCounter = "hatch_counter"
+        case hasGenderDifferences = "has_gender_differences"
+        case formsSwitchable = "forms_switchable"
+        case growthRate = "growth_rate"
         case pokedexNumbers = "pokedex_numbers"
+        case eggGroups = "egg_groups"
+        case color
+        case shape
+        case evolvesFromSpecies = "evolves_from_species"
+        case evolutionChain = "evolution_chain"
+        case habitat
+        case generation
+        case names
+        case flavorTextEntries = "flavor_text_entries"
+        case formDescriptions = "form_descriptions"
+        case genera
+        case varieties
     }
 }
 
