1b3aaa872b39c2a1493e1ec8fc1b98ed1e18de9e
taek0622 <min4644@gmail.com>
2024-08-24 11:38:52 +0900

    Feat: #18 Add PokemonGameVersion enum
    - 포켓몬 게임 버전을 구분하기 위한 Enum 추가

---
+++ PokemonDex/PokemonGameVersion.swift
@@ -0,0 +1,55 @@
+//
+//  PokemonGameVersion.swift
+//  PokemonDex
+//
+//  Created by 김민택 on 8/23/24.
+//
+
+import UIKit
+
+enum PokemonGameVersion: String, CaseIterable {
+    case red = "red"
+    case blue = "blue"
+    case yellow = "yellow"
+    case gold = "gold"
+    case silver = "silver"
+    case crystal = "crystal"
+    case ruby = "ruby"
+    case sapphire = "sapphire"
+    case emerald = "emerald"
+    case fireRed = "firered"
+    case leafGreen = "leafgreen"
+    case diamond = "diamond"
+    case pearl = "pearl"
+    case platinum = "platinum"
+    case heartGold = "heartgold"
+    case soulSilver = "soulsilver"
+    case black = "black"
+    case white = "white"
+    case colosseum = "colosseum"
+    case xd = "XD"
+    case black2 = "black-2"
+    case white2 = "white-2"
+    case x = "x"
+    case y = "y"
+    case omegaRuby = "omega-ruby"
+    case alphaSapphire = "alpha-sapphire"
+    case sun = "sun"
+    case moon = "moon"
+    case ultraSun = "ultra-sun"
+    case ultraMoon = "ultra-moon"
+    case lgp = "lets-go-pikachu"
+    case lge = "lets-go-eevee"
+    case sword = "sword"
+    case shield = "shield"
+    case theIsleOfArmor = "the-isle-of-armor"
+    case theCrownTundra = "the-crown-tundra"
+    case brilliantDiamond = "brilliant-diamond"
+    case shiningPearl = "shining-pearl"
+    case legendsArceus = "legends-arceus"
+    case scarlet = "scarlet"
+    case violet = "violet"
+    case theTealMask = "the-teal-mask"
+    case theIndigoDisk = "the-indigo-disk"
+
+}
